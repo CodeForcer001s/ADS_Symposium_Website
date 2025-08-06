db9a3e3f69e391ea55708d0c5512b5ce40b56439
Jegannath <sjegannath56@gmail.com>
2025-08-06 09:30:48 +0530

    reverted

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vercel from "vite-plugin-vercel";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), vercel()],
-  server: {
-    port: 5173,
-    host: true, // optional: makes it accessible over LAN if needed
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
