30d1ea57514a1601ef5040e692ec3c6cb5ba5de5
Jegannath <sjegannath56@gmail.com>
2025-08-06 09:17:16 +0530

    vercel errors resolved

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
+  server: {
+    port: 5173,
+    host: true, // optional: makes it accessible over LAN if needed
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
